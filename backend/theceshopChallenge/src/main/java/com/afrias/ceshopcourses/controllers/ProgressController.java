d3e5ed6139cb233f7f1f9a0cc22465e3fc61f651
Andrés Frías <inf512.andres.frias@gmail.com>
2023-02-04 15:06:55 -0400

    Update ProgressController.java

--- backend/theceshopChallenge/src/main/java/com/afrias/ceshopcourses/controllers/ProgressController.java
+++ backend/theceshopChallenge/src/main/java/com/afrias/ceshopcourses/controllers/ProgressController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.afrias.ceshopcourses.models.Progress;
 import com.afrias.ceshopcourses.repository.IProgressRepository;
 
-@CrossOrigin(origins = {"http://localhost:4200", "*", "http://localhost:8080", "http://localhost:8080/api/student"})
+@CrossOrigin(origins = {"http://localhost:4200", "*", "http://localhost:8080"})
 @RestController
 @RequestMapping("/api")
 public class ProgressController {
